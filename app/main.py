ba0ae1e258927d9d06d305e6264a27f3cdb7aeca
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-01-08 09:01:23 +0200

    Created BaseBook abstractclass with check_type, display, display_console, display_reverse, print_console, print_reverse abstractmethods. Updated display method in Book class. Created display_console, display_reverse, print, print_console, print_reverse methods in Book class. Updated main function.

--- app/main.py
+++ app/main.py
@@ -1,29 +1,69 @@
 import json
 import xml.etree.ElementTree as ET
+from abc import ABC, abstractmethod
 
 
-class Book:
+class BaseBook(ABC):
+    DISPLAY_TYPES = ("console", "reverse")
+
+    def check_type(self, command,  type_to_check: str):
+        if type_to_check not in self.DISPLAY_TYPES:
+            raise ValueError(f"Unknown {command} type: {type_to_check}")
+
+    @abstractmethod
+    def display(self, display_type):
+        ...
+
+    @abstractmethod
+    def display_console(self):
+        ...
+
+    @abstractmethod
+    def display_reverse(self):
+        ...
+
+    @abstractmethod
+    def print(self, print_type):
+        ...
+
+    @abstractmethod
+    def print_console(self):
+        ...
+
+    @abstractmethod
+    def print_reverse(self):
+        ...
+
+
+class Book(BaseBook):
+
     def __init__(self, title: str, content: str):
         self.title = title
         self.content = content
 
     def display(self, display_type: str) -> None:
-        if display_type == "console":
-            print(self.content)
-        elif display_type == "reverse":
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown display type: {display_type}")
+        self.check_type("display", display_type)
 
-    def print_book(self, print_type: str) -> None:
-        if print_type == "console":
-            print(f"Printing the book: {self.title}...")
-            print(self.content)
-        elif print_type == "reverse":
-            print(f"Printing the book in reverse: {self.title}...")
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown print type: {print_type}")
+        getattr(self, f"display_{display_type}")()
+
+    def display_console(self):
+        print(self.content)
+
+    def display_reverse(self):
+        print(self.content[::-1])
+
+    def print(self, print_type):
+        self.check_type("print", print_type)
+
+        getattr(self, f"print_{print_type}")()
+
+    def print_console(self):
+        print(f"Printing the book: {self.title}...")
+        print(self.content)
+
+    def print_reverse(self):
+        print(f"Printing the book in reverse: {self.title}...")
+        print(self.content[::-1])
 
     def serialize(self, serialize_type: str) -> str:
         if serialize_type == "json":
@@ -42,13 +82,20 @@ class Book:
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd == "display":
-            book.display(method_type)
+            getattr(book, f"{cmd}_{method_type}")()
         elif cmd == "print":
-            book.print_book(method_type)
+            getattr(book, f"{cmd}_{method_type}")()
         elif cmd == "serialize":
             return book.serialize(method_type)
 
 
 if __name__ == "__main__":
     sample_book = Book("Sample Book", "This is some sample content.")
-    print(main(sample_book, [("display", "reverse"), ("serialize", "xml")]))
+    print(
+        main(sample_book,
+             [
+                 ("display", "reverse"),
+                 ("serialize", "xml")
+             ]
+             )
+    )
